40aef5980022e8aa44333c191bf72813bb495708
WhiteDeveloper <aliksieiev.yurii@gmail.com>
2019-03-01 20:20:33 +0100

    git_res

--- app/src/main/java/com/yuriialieksieiev/mvvmexample/MainActivity.java
+++ app/src/main/java/com/yuriialieksieiev/mvvmexample/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
-import android.media.browse.MediaBrowser;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
